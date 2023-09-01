90c65c41471dd44a6e2b98bcc651614b4a9e1582
zdrzalikprzemyslaw <zdrzalikprzemyslaw@gmail.com>
2023-09-01 19:39:55 +0200

    run in background

--- rebuild-docker.sh
+++ rebuild-docker.sh
@@ -4,4 +4,4 @@ docker compose -f dockercompose.yml down --volumes  --remove-orphans
 
 docker compose -f dockercompose.yml build --no-cache
 
-docker compose -f dockercompose.yml up
+docker compose -f dockercompose.yml up -d
